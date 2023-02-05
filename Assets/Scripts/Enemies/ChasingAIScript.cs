59e85e2bdc4673b01d6ad25cd5adfd4030f2e392
Virgil <virgil@nosoft.io>
2023-02-05 14:50:39 +0100

    two first enemies

--- Assets/Scripts/Enemies/ChasingAIScript.cs
+++ Assets/Scripts/Enemies/ChasingAIScript.cs
@@ -4,13 +4,16 @@ using UnityEngine.AI;
 public class ChasingAIScript : MonoBehaviour {
     NavMeshAgent agent;
     [SerializeField] EnemyHealth script;
+    [SerializeField] Animator animator;
+    public bool isStatic = false;
     private void Init() {
         agent = GetComponent<NavMeshAgent>();
     }
 
     private void Update() {
         transform.LookAt(ManagePlayer.player);
-        if (agent) {
+        if (!isStatic && agent) {
+            if(animator) animator.SetFloat("Velocity", agent.velocity.magnitude);
             if (!script.isGoingToDie) agent.SetDestination(ManagePlayer.player.position);
             else agent.SetDestination(transform.position);
         } else Init();
