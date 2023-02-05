99c4e7758c9a468df7912710efc860aa3eb9793d
KAlecs12 <alexandre.bruno@ynov.com>
2023-02-05 16:59:04 +0100

    spawn waves N

--- Assets/Scripts/Enemies/SpawnSystem.cs
+++ Assets/Scripts/Enemies/SpawnSystem.cs
@@ -11,29 +11,29 @@ public class SpawnSystem : MonoBehaviour
     public GameObject bigRoot;
     public GameObject rangeRoot;
     public GameObject fastRoot;
-    [Header("Wave 1")] 
+    [Header("Wave 1:")] 
     public int enemy1Count;
     public int enemy1Number;
-    [Header("Wave 2")]
+    [Header("Wave 2:")]
     public int enemy2Count;
     public int enemy2Number;
     public int enemyBig2Count;
     public int enemyBig2Number;
-    [Header("Wave 3")]
+    [Header("Wave 3:")]
     public int enemy3Count;
     public int enemy3Number;
     public int enemyRange3Count;
     public int enemyRange3Number;
     public int enemyBig3Count;
     public int enemyBig3Number;
-    [Header("Wave 4")]
+    [Header("Wave 4:")]
     public int enemy4Count;
     public int enemy4Number;
     public int enemyFast4Count;
     public int enemyFast4Number;
     public int enemyBig4Count;
     public int enemyBig4Number;
-    [Header("Wave 5")]
+    [Header("Wave 5:")]
     public int enemy5Count;
     public int enemy5Number;
     public int enemyRange5Count;
@@ -42,20 +42,34 @@ public class SpawnSystem : MonoBehaviour
     public int enemyFast5Number;
     public int enemyBig5Count;
     public int enemyBig5Number;
-    
-    
-    [Header("Total d'enemies tués")]
+    [Header("Next Wave:")]
+    public int enemyNCount;
+    public int enemyNNumber;
+    public int enemyRangeNCount;
+    public int enemyRangeNNumber;
+    public int enemyFastNCount;
+    public int enemyFastNNumber;
+    public int enemyBigNCount;
+    public int enemyBigNNumber;
+
+
+    [Header("Total d'enemies tués:")]
     public int totalEnemyKilled;
     public int totalEnemy1Killed;
     public int totalEnemy2Killed;
     public int totalEnemy3Killed;
     public int totalEnemy4Killed;
     
+    [Header("Stats:")]
+    private int enemyNumber;
+    public int waveNumber = 5;
+    
     private Boolean _waveOne = true;
     private Boolean _waveTwo = false;
     private Boolean _waveThree = false;
     private Boolean _waveFour = false;
     private Boolean _waveFive = false;
+    private Boolean _nextWave = false;
 
     private List<GameObject> tinyRootL = new List<GameObject>();
     private List<GameObject> bigRootL = new List<GameObject>();
@@ -113,7 +127,7 @@ public class SpawnSystem : MonoBehaviour
         
         if (_waveOne)
         {
-            if (totalEnemyKilled > 10)
+            if (totalEnemyKilled == 5)
             {
                 _waveOne = false;
                 Debug.Log("Start Wave 2");
@@ -124,7 +138,7 @@ public class SpawnSystem : MonoBehaviour
         if (_waveTwo)
         {
 
-            if (totalEnemyKilled > 25 && totalEnemy2Killed == 5)
+            if (totalEnemyKilled > 20 && totalEnemy2Killed == 5)
             {
                 _waveTwo = false;
                 Debug.Log("Start Wave 3");
@@ -135,7 +149,7 @@ public class SpawnSystem : MonoBehaviour
         if (_waveThree)
         {
 
-            if (totalEnemyKilled > 45 && totalEnemy3Killed == 5)
+            if (totalEnemyKilled > 40 && totalEnemy3Killed == 5)
             {
                 _waveThree = false;
                 Debug.Log("Start Wave 4");
@@ -146,7 +160,7 @@ public class SpawnSystem : MonoBehaviour
         if (_waveFour)
         {
 
-            if (totalEnemyKilled > enemy2Number-5 && totalEnemy2Killed == 5)
+            if (totalEnemyKilled > 47 && totalEnemy4Killed == 5)
             {
                 _waveFour = false;
                 Debug.Log("Start Wave 5");
@@ -154,7 +168,28 @@ public class SpawnSystem : MonoBehaviour
                 StartCoroutine(EnemyDrop());
             }
         }
-
+        if (_waveFive)
+        {
+            if (totalEnemyKilled > 77)
+            {
+                _waveFive = false;
+                Debug.Log("Start Wave 5");
+                _nextWave = true;
+                enemyNumber = totalEnemyKilled;
+                StartCoroutine(EnemyDrop());
+            }
+        }
+        if (_nextWave)
+        {
+            waveNumber += 1;
+            if (totalEnemyKilled > enemyNumber - 5)
+            {
+                Debug.Log("Start Wave " + waveNumber);
+                
+                enemyNumber = totalEnemyKilled;
+                StartCoroutine(EnemyDrop());
+            }
+        }
     }
     
     IEnumerator EnemyDrop()
@@ -396,7 +431,7 @@ public class SpawnSystem : MonoBehaviour
             }
         }
             
-        /*  ----------------   WAVE FOUR  -------------------*/
+        /*  ----------------   WAVE FIVE  -------------------*/
             
         if (_waveFive)
         {
@@ -489,6 +524,123 @@ public class SpawnSystem : MonoBehaviour
                         fastRootL.Add(Instantiate(fastRoot, objectPosition, Quaternion.identity));
                         yield return new WaitForSeconds(0.1f);
                         enemyRange3Count += 1;
+                        objectSpawned = true;
+                    }
+
+                }
+            }
+        }
+        
+         /*  ----------------   WAVE N  -------------------*/
+            
+        if (_nextWave)
+        {
+            enemyNNumber = Random.Range(10, 15);
+            enemyBigNNumber = Random.Range(10, 25);
+
+            if (enemyBigNNumber < 15)
+            {
+                enemyRangeNNumber = Random.Range(15, 25);
+            } else {
+                enemyRangeNNumber = Random.Range(10, 15);
+            }
+            
+            if (enemyRangeNNumber < 15)
+            {
+                enemyFastNNumber = Random.Range(15, 25);
+            } else {
+                enemyFastNNumber = Random.Range(10, 15);
+            }
+
+            while (enemyNCount < enemyNNumber)
+            {
+                bool objectSpawned = false;
+                while (!objectSpawned)
+                {
+
+                    Vector3 objectPosition = new Vector3(transform.position.x + Random.Range(-50, 50), 0,
+                        transform.position.z + Random.Range(-50, 50));
+
+                    if ((objectPosition - transform.position).magnitude < 40 || Physics.CheckSphere(objectPosition, 0))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        tinyRootL.Add(Instantiate(simpleRoot, objectPosition, Quaternion.identity));
+                        yield return new WaitForSeconds(0.1f);
+                        enemyNCount += 1;
+                        objectSpawned = true;
+                    }
+
+                }
+            }
+
+            while (enemyRangeNCount < enemyRangeNNumber)
+            {
+                bool objectSpawned = false;
+                while (!objectSpawned)
+                {
+
+                    Vector3 objectPosition = new Vector3(transform.position.x + Random.Range(-50, 50), 0,
+                        transform.position.z + Random.Range(-50, 50));
+
+                    if ((objectPosition - transform.position).magnitude < 40 || Physics.CheckSphere(objectPosition, 0))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        rangeRootL.Add(Instantiate(rangeRoot, objectPosition, Quaternion.identity));
+                        yield return new WaitForSeconds(0.1f);
+                        enemyRangeNCount += 1;
+                        objectSpawned = true;
+                    }
+
+                }
+            }
+            while (enemyBigNCount < enemyBigNNumber)
+            {
+                bool objectSpawned = false;
+                while (!objectSpawned)
+                {
+
+                    Vector3 objectPosition = new Vector3(transform.position.x + Random.Range(-50, 50), 0,
+                        transform.position.z + Random.Range(-50, 50));
+
+                    if ((objectPosition - transform.position).magnitude < 40 || Physics.CheckSphere(objectPosition, 0))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        bigRootL.Add(Instantiate(bigRoot, objectPosition, Quaternion.identity));
+                        yield return new WaitForSeconds(0.1f);
+                        enemyRangeNCount += 1;
+                        objectSpawned = true;
+                    }
+
+                }
+            }
+                
+            while (enemyFastNCount < enemyFastNNumber)
+            {
+                bool objectSpawned = false;
+                while (!objectSpawned)
+                {
+
+                    Vector3 objectPosition = new Vector3(transform.position.x + Random.Range(-50, 50), 0,
+                        transform.position.z + Random.Range(-50, 50));
+
+                    if ((objectPosition - transform.position).magnitude < 40 || Physics.CheckSphere(objectPosition, 0))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        fastRootL.Add(Instantiate(fastRoot, objectPosition, Quaternion.identity));
+                        yield return new WaitForSeconds(0.1f);
+                        enemyRangeNCount += 1;
                         objectSpawned = true;
                     }
 
