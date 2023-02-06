1241034e8efa675537f474cbb0bfadd4419fc978
UnaChevrotina <romain.lecuyer@ynov.com>
2023-02-06 12:18:50 +0100

    Objet Heal + bug fix spawn plusieurs soin sur GloryKill

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -31,16 +31,18 @@ public class Enemy : MonoBehaviour
     private void FixedUpdate() {
         transform.rotation = Quaternion.LookRotation(ManagePlayer.player.position.UpdateAxis(transform.position.y, VectorAxis.Y) - transform.position, transform.up);
         if (!isGoingToDie) return;
-        if (animator.GetCurrentAnimatorStateInfo(0).IsName("RootExplode")) {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("RootExplode"))
+        {
             ManagePlayer.ChangeLockState(false);
             transform.LookAt(ManagePlayer.player);
             Instantiate(explosion, transform.position.UpdateAxis(transform.position.y + 0.75f, VectorAxis.Y), explosion.transform.rotation * transform.rotation);
-            Vector3 position = transform.position;
-            Debug.Log("Position : " + position);
-            GameObject collectible = Instantiate(collectiblePrefab);
-            collectible.transform.position = position;
         }
-        else if (animator.GetCurrentAnimatorStateInfo(0).IsName("isDead")) Destroy(gameObject);
+        else if (animator.GetCurrentAnimatorStateInfo(0).IsName("isDead"))
+        {
+            Destroy(gameObject);
+            GameObject collectible = Instantiate(collectiblePrefab);
+            collectible.transform.position = transform.position;
+        }
     }
     private void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.layer == Mathf.Log(Player.value, 2)) {
--- Assets/Scripts/Props/HealCollectible.cs
+++ Assets/Scripts/Props/HealCollectible.cs
@@ -2,12 +2,11 @@ using UnityEngine;
 
 public class HealCollectible : MonoBehaviour
 {
-
+    [SerializeField] LayerMask PlayerMask;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.gameObject.layer == Mathf.Log(PlayerMask.value, 2))
         {
-
             int healValue = 30;
             Debug.Log("Test");
             other.gameObject.GetComponent<Health>().TakeHeal(healValue);
